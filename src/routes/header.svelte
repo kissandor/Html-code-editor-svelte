a3f1752e19bf61f556882fda8d63339cec5b3ba6
Sandor Kis <kisandor@hotmail.com>
2023-02-04 01:02:29 +0000

    demo button implemented on header

--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -1,11 +1,38 @@
-<div>HTML Code editor</div>
+<div>
+    <span>HTML Code editor</span>
+    <button>Demo</button>
+</div>
+
 <style>
     div {
         width:100%;
-        height: 50px;
+        height: 40px;
         background-color: #914C53;
-        color: white;
+        
         padding: none;
         margin: none;
     }
+    span{
+        margin-left: 50px;
+        vertical-align: middle;
+        line-height: 40px;
+        color: white;
+        font-weight: bold;
+        font-size:x-large;
+        font-family:Georgia, 'Times New Roman', Times, serif      
+    }
+    button{
+        margin-left: 50px;
+        height: 40px;
+        background-color: #914C53;
+        border: none;
+        color: white;
+        font-size:medium;
+        font-weight: bold;
+    }
+    button:hover{
+        background-color: #633035;
+        cursor: pointer;
+    }
+
 </style>
