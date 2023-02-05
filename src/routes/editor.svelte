e8f9494207196e64de4fcd4c1e994659adf599f9
Sandor Kis <kisandor@hotmail.com>
2023-02-05 22:28:12 +0000

    html tag ribbon has been implemented

--- src/routes/editor.svelte
+++ src/routes/editor.svelte
@@ -1,9 +1,11 @@
 <script>
+    import Ribbon from "./ribbon.svelte";
    export let value="<strong><i>Type the HTML code here...</i></strong>";
 </script>
 
 <div class="parent">
     <div class="left child">
+        <Ribbon />
         <textarea bind:value={value} ></textarea>
     </div>
     <div class="right child">
@@ -14,22 +16,29 @@
 
 <style>
 	textarea { 
-        width: 99%; 
-        height: 390px;
+        width: 96%; 
+        height: 360px;
         resize: none; 
         border: none;
         outline: none;
+        margin: 10px;
+    }
+    
+    .parent{
+        margin-top: 20px;
+        width: 100%;
     }
    
     .child{
         height:400px;
-        width: 46%;
+        width: 47%;
         display: inline-block;
         vertical-align: middle;
-        margin-left: 20px;
-        margin-top: 20px;
-        padding: 10px;
+        margin-left: 25px;
         overflow-wrap:break-word;
         background-color: white;
+    }
+    .right{
+        padding:10px;
     }
 </style>
---
+++ src/routes/ribbon.svelte
@@ -0,0 +1,33 @@
+<div class="ribbon"> 
+    <ul>
+        <li>h1</li>
+        <li>h2</li>
+        <li>h3</li>
+        <li>br</li> 
+        <li>div</li>
+        <li>ul</li>
+        <li>li</li>
+      </ul>
+</div>
+
+<style>
+    ul {
+      list-style-type: none;
+      margin: 0;
+      padding: 0;
+      overflow: hidden;
+      background-color: #914C53;
+      color: white;
+    }
+    
+    li {
+      float: left;
+      padding: 7px 13px;
+    }    
+
+    li:hover{
+        background-color: #633035;
+        cursor: pointer;
+    }
+
+</style>
